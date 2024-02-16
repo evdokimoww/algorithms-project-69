ea72aed21b071619cab05e1de5d12f1baef95c22
aevdokimov <a.evdokimov@t1-group.ru>
2024-02-16 16:19:27 +0300

    lesson5

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import sum from 'lodash/sum.js';
-
 const getMatchedWordsArray = (text) => text.match(/\w+/g).map((el) => el.toLowerCase());
 
 const sortSearchResult = (a, b) => {
-  const aValues = Object.values(a[1]);
-  const bValues = Object.values(b[1]);
+  const aDocCount = a[1][0];
+  const bDocCount = b[1][0];
 
-  if (aValues.length > bValues.length) {
+  const aWordCount = a[1][1];
+  const bWordCount = b[1][1];
+
+  if (aDocCount > bDocCount) {
     return -1;
   }
 
-  if (aValues.length < bValues.length) {
+  if (aDocCount < bDocCount) {
     return 1;
   }
 
-  if (aValues.length === bValues.length) {
-    if (sum(aValues) > sum(bValues)) {
+  if (aDocCount === bDocCount) {
+    if (aWordCount > bWordCount) {
       return -1;
     }
 
-    if (sum(aValues) < sum(bValues)) {
+    if (aWordCount < bWordCount) {
       return 1;
     }
   }
@@ -32,29 +33,39 @@ const search = (docs, searchString) => {
     return [];
   }
 
-  const result = {};
-  const cleanSearchString = getMatchedWordsArray(searchString);
+  const indexes = {};
 
   for (let i = 0; i < docs.length; i += 1) {
     const cleanText = getMatchedWordsArray(docs[i].text);
 
-    for (let j = 0; j < cleanSearchString.length; j += 1) {
-      if (cleanText.includes(cleanSearchString[j])) {
-        result[docs[i].id] = {
-          ...result[docs[i].id],
-          [cleanSearchString[j]]: 0,
+    cleanText.forEach((el) => {
+      if (indexes[el]) {
+        indexes[el] = {
+          ...indexes[el],
+          [docs[i].id]: indexes[el][docs[i].id] ? indexes[el][docs[i].id] + 1 : 1,
         };
-
-        for (let k = 0; k < cleanText.length; k += 1) {
-          if (cleanText[k] === cleanSearchString[j]) {
-            result[docs[i].id][cleanSearchString[j]] += 1;
-          }
-        }
+      } else {
+        indexes[el] = { [docs[i].id]: 1 };
       }
-    }
+    });
   }
 
-  return Object.entries(result).sort(sortSearchResult).map((el) => el[0]);
+  const cleanSearchString = getMatchedWordsArray(searchString);
+  const result = Object.entries(indexes).filter((el) => cleanSearchString.includes(el[0]));
+
+  const sorted = {};
+
+  result.map((el) => el[1]).forEach((el) => {
+    Object.entries(el).forEach((el2) => {
+      if (sorted[el2[0]]) {
+        sorted[el2[0]] = [sorted[el2[0]][0] + 1, sorted[el2[0]][1] + el2[1]];
+      } else {
+        sorted[el2[0]] = [1, el2[1]];
+      }
+    });
+  });
+
+  return Object.entries(sorted).sort(sortSearchResult).map((el) => el[0]);
 };
 
 export default search;
