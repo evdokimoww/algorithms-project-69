1020ee1f3528f38a22c8d1600e2498c175c116b9
aevdokimov <a.evdokimov@t1-group.ru>
2024-02-16 15:07:07 +0300

    lesson4

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -7,12 +7,13 @@ const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!
 const doc2 = { id: 'doc2', text: "Don't shoot that thing at me." };
 const doc3 = { id: 'doc3', text: "I'm your shooter." };
 const doc4 = { id: 'doc4', text: "Don't shoot shoot shoot shoot that thing at me." };
-const doc5 = { id: 'doc2', text: "Don't shoot shoot that thing at me." };
+const doc5 = { id: 'doc5', text: "Don't shoot shoot that thing at me." };
+
 const docs = [doc1, doc2, doc3];
 const docsForRelevant = [doc1, doc3, doc4, doc5];
 
 test('search', () => {
-  expect(search(docs, 'shoot')).toStrictEqual(['doc1', 'doc2']);
+  expect(search(docs, 'shoot')).toContain('doc1', 'doc2');
 });
 
 test('emptysearch', () => {
@@ -20,5 +21,9 @@ test('emptysearch', () => {
 });
 
 test('relevant', () => {
-  expect(search(docsForRelevant, 'shoot')).toStrictEqual(['doc4', 'doc2', 'doc1']);
+  expect(search(docsForRelevant, 'shoot')).toStrictEqual(['doc4', 'doc5', 'doc1']);
+});
+
+test('fuzzy', () => {
+  expect(search(docsForRelevant, 'shoot that thing')).toStrictEqual(['doc4', 'doc5', 'doc1']);
 });
--- src/index.js
+++ src/index.js
@@ -1,26 +1,60 @@
-const search = (docs, word) => {
+import sum from 'lodash/sum.js';
+
+const getMatchedWordsArray = (text) => text.match(/\w+/g).map((el) => el.toLowerCase());
+
+const sortSearchResult = (a, b) => {
+  const aValues = Object.values(a[1]);
+  const bValues = Object.values(b[1]);
+
+  if (aValues.length > bValues.length) {
+    return -1;
+  }
+
+  if (aValues.length < bValues.length) {
+    return 1;
+  }
+
+  if (aValues.length === bValues.length) {
+    if (sum(aValues) > sum(bValues)) {
+      return -1;
+    }
+
+    if (sum(aValues) < sum(bValues)) {
+      return 1;
+    }
+  }
+
+  return 0;
+};
+
+const search = (docs, searchString) => {
   if (docs.length === 0) {
     return [];
   }
 
   const result = {};
+  const cleanSearchString = getMatchedWordsArray(searchString);
 
   for (let i = 0; i < docs.length; i += 1) {
-    const cleanString = docs[i].text.match(/\w+/g).map((el) => el.toLowerCase());
-    const cleanWord = word.match(/\w+/g)[0].toLowerCase();
+    const cleanText = getMatchedWordsArray(docs[i].text);
 
-    if (cleanString.includes(cleanWord)) {
-      result[docs[i].id] = 0;
+    for (let j = 0; j < cleanSearchString.length; j += 1) {
+      if (cleanText.includes(cleanSearchString[j])) {
+        result[docs[i].id] = {
+          ...result[docs[i].id],
+          [cleanSearchString[j]]: 0,
+        };
 
-      for (let j = 0; j < cleanString.length; j += 1) {
-        if (cleanString[j] === cleanWord) {
-          result[docs[i].id] += 1;
+        for (let k = 0; k < cleanText.length; k += 1) {
+          if (cleanText[k] === cleanSearchString[j]) {
+            result[docs[i].id][cleanSearchString[j]] += 1;
+          }
         }
       }
     }
   }
 
-  return Object.entries(result).sort((a, b) => b[1] - a[1]).map((el) => el[0]);
+  return Object.entries(result).sort(sortSearchResult).map((el) => el[0]);
 };
 
 export default search;
