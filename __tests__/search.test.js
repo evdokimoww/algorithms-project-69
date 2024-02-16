dde26164dc4c506fab078a7aa5239ac7d557d771
aevdokimov <a.evdokimov@t1-group.ru>
2024-02-16 11:03:33 +0300

    lesson3

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -4,9 +4,12 @@ import { test, expect } from '@jest/globals';
 import search from '../index.js';
 
 const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+const doc2 = { id: 'doc2', text: "Don't shoot that thing at me." };
 const doc3 = { id: 'doc3', text: "I'm your shooter." };
+const doc4 = { id: 'doc4', text: "Don't shoot shoot shoot shoot that thing at me." };
+const doc5 = { id: 'doc2', text: "Don't shoot shoot that thing at me." };
 const docs = [doc1, doc2, doc3];
+const docsForRelevant = [doc1, doc3, doc4, doc5];
 
 test('search', () => {
   expect(search(docs, 'shoot')).toStrictEqual(['doc1', 'doc2']);
@@ -14,4 +17,8 @@ test('search', () => {
 
 test('emptysearch', () => {
   expect(search([], 'shoot')).toStrictEqual([]);
+});
+
+test('relevant', () => {
+  expect(search(docsForRelevant, 'shoot')).toStrictEqual(['doc4', 'doc2', 'doc1']);
 });
--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ const search = (docs, word) => {
     return [];
   }
 
-  const result = [];
+  const result = {};
 
   for (let i = 0; i < docs.length; i += 1) {
     const cleanString = docs[i].text.match(/\w+/g).map((el) => el.toLowerCase());
     const cleanWord = word.match(/\w+/g)[0].toLowerCase();
 
     if (cleanString.includes(cleanWord)) {
-      result.push(docs[i].id);
+      result[docs[i].id] = 0;
+
+      for (let j = 0; j < cleanString.length; j += 1) {
+        if (cleanString[j] === cleanWord) {
+          result[docs[i].id] += 1;
+        }
+      }
     }
   }
 
-  return result;
+  return Object.entries(result).sort((a, b) => b[1] - a[1]).map((el) => el[0]);
 };
 
 export default search;
